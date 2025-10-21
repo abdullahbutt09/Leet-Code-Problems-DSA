603aef563fe75686708e7cc85739ce5575bac7f7
Abdullah Butt <abdullah.butt.dev.github@gmail.com>
2025-10-21 13:25:13 +0500

    added some test cases for binary search

--- Arrays-Questions/binary_search.cpp
+++ Arrays-Questions/binary_search.cpp
@@ -33,18 +33,23 @@ int main(){
 
     cout << endl;
     // int arr[] = {1,2,4,5,6,7,9,10};
-    int arr[] = {1,2,4,5,6,7,9,10,12};
+    // int arr[] = {3, 5, 12, 19, 22, 56, 78, 90};
+    // int arr[] = {4, 9, 16, 18, 23, 34, 54, 95};
+    // int arr[] = {2, 11, 15, 33, 45, 67, 76, 89};
+    // int arr[] = {5, 14, 21, 29, 31, 44, 70, 80};
+    // int arr[] = {6, 17, 24, 39, 50, 63, 77, 99};
+    // int arr[] = {8, 10, 26, 32, 41, 52, 68, 85};
+    // int arr[] = {7, 13, 19, 30, 38, 48, 59, 93};
+    // int arr[] = {1, 12, 20, 27, 36, 47, 62, 79};
+    // int arr[] = {3, 18, 22, 35, 42, 55, 64, 88};
+    int arr[] = {9, 15, 28, 37, 49, 60, 71, 92};
+    
     int size = sizeof(arr) / sizeof(arr[0]);
-    int key = 12;
+    int key = 92;
     
     bool result = binarySearch(arr, size, key);
     
-    if(result){
-        cout << "PRESENT!";
-    }
-    else {
-        cout << "ABSENT!";
-    }
+    cout << (result ? "PRESENT" : "ABSENT");
     cout << endl;
     return 0;
 }
