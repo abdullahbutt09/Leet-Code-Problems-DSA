b9c6f83d1c73bfc2ab62c4ba3b5664c52dc0b352
Abdullah Butt <abdullah.butt.dev.github@gmail.com>
2025-11-13 11:10:14 +0500

    added a question of square root using binary search

---
+++ Searching-Questions/square_root_using_bs.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+//square root using binary search
+
+long long int square_root(int number){
+
+    int start = 0;
+    int end = number;
+
+    long long int mid = start + (end - start) / 2;
+
+    while (start <= end)
+    {
+        long long int square = mid * mid;
+
+        if(square == number){
+            return mid;
+        }
+        
+        else if(square < number){
+            start = mid + 1;
+        }
+
+        else {
+            end = mid - 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return mid;
+}
+
+int main(){ 
+    
+    cout << endl;
+
+    int result = square_root(1000000);
+
+    cout << result << endl;
+    
+    return 0;
+}
+
+//* added long long to handle maximum integer values to get the square root
+
+//! try to remove long long and try to get the answer of 1000000 you will encounter invalid answer
+
+/* 
+
+* sqrt(1,000,000)
+! mid ≈ 500,000
+? mid * mid = 250,000,000,000  (which exceeds int range)
+
+*/
